01a4cdfaf82b3dc5c8f2420e78f3a619aaf07257
kokoroteam <89568186+kokoroteam@users.noreply.github.com>
2021-09-05 22:37:12 -0700

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -7,6 +7,7 @@
 
 c.Appmode.show_edit_button = False
 c.Appmode.show_visit_button = False
+c.Appmode.show_other_buttons = False
 
 ## The date format used by logging formatters for %(asctime)s
 #  Default: '%Y-%m-%d %H:%M:%S'
